e52935d97c47ee8c2e0b8901ede4920d02dda197
VolletMaxence <m.vollet@la-providence.net>
2021-06-03 11:57:29 +0200

    Correction titre page Cérémonie

--- Site/Ceremonie/Ceremonie.php
+++ Site/Ceremonie/Ceremonie.php
@@ -6,7 +6,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Au Grain de Pollen - Baptheme</title>
+        <title>Au Grain de Pollen - Cérémonie</title>
     </head>
     <body>
         <?php 
