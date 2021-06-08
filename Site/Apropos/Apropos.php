195d87a7a88184227a4db31de2071fc08220ca68
VolletMaxence <m.vollet@la-providence.net>
2021-06-08 17:58:03 +0200

    Ratage menu pour version Portable

--- Site/Apropos/Apropos.php
+++ Site/Apropos/Apropos.php
@@ -11,6 +11,19 @@
     <body>
         <?php 
             include "../../menu.php"; 
+
+            date_default_timezone_set('Europe/Paris');
+            $date = date('D m y G:i:s');
+            echo $date;
+
+            /*
+            Du mardi au vendredi : de 9h à 12h30 et de 14h30 à 17h45
+
+            Samedi : de 9h à 19h45
+
+            Dimanche : de 9h à 12h30
+            */
+            
         ?>
     </body>
 </html>
